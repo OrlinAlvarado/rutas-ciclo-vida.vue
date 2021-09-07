f6b3cf1253f5cfd9fe83b7cdc9d7b0a9ba29acfe
Orlin Alvarado <oalvaradomorales@gmail.com>
2021-09-07 07:28:47 -0600

    Rutas y ciclo de vida Segundo Layout finalizado

---
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -0,0 +1,71 @@
+<template>
+  <h1>About Page</h1>
+
+  <h2>{{ name }}</h2>
+  <button @click="onChangeName">
+    Cambiar nombre
+  </button>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Orlin'
+    }
+  },
+  methods: {
+    onChangeName(){
+      this.name = 'Yaneth'
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    this.name = 'David'
+    console.log('created')
+
+    //Peticiones HTTP
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+  },
+  errorCaptured() {
+    console.log('errorCaptured')
+  },
+  renderTracked() {
+    console.log('renderTracked')
+  },
+  renderTriggered() {
+    console.log('renderTriggered')
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <!-- <router-link :to="{ name: 'home' }">Pokemon List</router-link>
+        <router-link :to="{ name: 'pokemon-id', params: { id: 85 }}">Pokemon por id</router-link>
+        <router-link :to="{ name: 'about' }">About</router-link> -->
+
+        <CustomLink 
+            v-for="link in links"
+            :key="link.to"
+            :link="link" 
+        />
+    </div>    
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    data(){
+        return {
+            links:[
+                { to: 'pokemon-home', name: 'Pokemons' },
+                { to: '/pokemonid/50', name: 'Por ID' },
+                { to: '/about', name: 'About' },
+                { to: 'https://google.com', name: 'Google' }
+            ]
+        }
+    },
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink'))
+    }
+}
+</script>
+
+<style scoped>
+    div {
+       padding: 0 30px; 
+    }
+
+    div a {
+        font-weight: bold;
+        color: #2c3e50;
+        margin: 0 10px;
+    }
+
+    /* div a.router-link-exact-active{
+        color: #42b983;
+    } */
+</style>
